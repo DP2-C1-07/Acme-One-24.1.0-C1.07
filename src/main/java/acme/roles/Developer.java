95423308c9e374844a6eb7571e2b98abe4bba34c
alfalolan <alfalolan@alum.us.es>
2024-03-02 18:47:17 +0100

    Developer role created

---
+++ src/main/java/acme/roles/Developer.java
@@ -0,0 +1,39 @@
+
+package acme.roles;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractRole;
+
+public class Developer extends AbstractRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@NotBlank
+	@Length(max = 75)
+	String						degree;
+
+	@NotBlank
+	@Length(max = 100)
+	String						specialisation;
+
+	@NotBlank
+	@Length(max = 100)
+	String						skills;
+
+	@NotNull
+	@Email
+	String						email;
+
+	@URL
+	String						link;
+
+}
