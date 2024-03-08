9687f2fbe44e747bfee4f0c4cb2ed013b7fd4c4f
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2024-03-08 19:45:07 +0100

    feat: create Invoice entity

---
+++ src/main/java/acme/entities/invoices/Invoice.java
@@ -0,0 +1,73 @@
+
+package acme.entities.invoices;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Invoice extends AbstractEntity {
+
+	//Serialisation identifier -----------------------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	//Attributes --------------------------------------------------------------------------------
+
+	@NotNull
+	@Column(unique = true)
+	@Pattern(regexp = "^IN-[0-9]{4}-[0-9]{4}$")
+	private String				code;
+
+	@NotNull
+	@PastOrPresent
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationTime;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	// TODO: at least one month ahead the registration time
+	private Date				dueDate;
+
+	@Positive
+	@Digits(integer = 10, fraction = 2)
+	private double				quantity;
+
+	@Min(0)
+	@Digits(integer = 10, fraction = 2)
+	private double				tax;
+
+	@NotBlank
+	private String				currency;
+
+	@URL
+	@Length(max = 255)
+	private String				link;
+
+
+	@Transient
+	public double getTotalAmount() {
+		return this.quantity * (1d + this.tax);
+	}
+
+}
