eb09e0f4e39f4ad00adda6697535974995f631e1
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2024-03-08 17:48:35 +0100

    fix: check whole string in the code pattern

--- src/main/java/acme/entities/sponsorships/Sponsorship.java
+++ src/main/java/acme/entities/sponsorships/Sponsorship.java
@@ -43,7 +43,7 @@ public class Sponsorship extends AbstractEntity {
 
 	@NotBlank
 	@Column(unique = true)
-	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	@Pattern(regexp = "^[A-Z]{1,3}-[0-9]{3}$")
 	private String				code;
 
 	@Past
