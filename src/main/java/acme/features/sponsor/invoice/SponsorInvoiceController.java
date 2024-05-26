985220f44846d187de498b80ec7844bffe74867a
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2024-05-26 13:19:26 +0200

    feat: implement sponsor/invoice/show

---
+++ src/main/java/acme/features/sponsor/invoice/SponsorInvoiceController.java
@@ -0,0 +1,29 @@
+
+package acme.features.sponsor.invoice;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.client.controllers.AbstractController;
+import acme.entities.sponsorships.Invoice;
+import acme.roles.Sponsor;
+
+@Controller
+public class SponsorInvoiceController extends AbstractController<Sponsor, Invoice> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private SponsorInvoiceListService listService;
+
+	// Constructors -----------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listService);
+		super.addCustomCommand("list-mine", "list", this.listService);
+	}
+}
---
+++ src/main/java/acme/features/sponsor/invoice/SponsorInvoiceRepository.java
@@ -0,0 +1,28 @@
+
+package acme.features.sponsor.invoice;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.sponsorships.Invoice;
+import acme.entities.sponsorships.Sponsorship;
+import acme.roles.Sponsor;
+
+@Repository
+public interface SponsorInvoiceRepository extends AbstractRepository {
+
+	@Query("select s from Sponsorship s where s.id = :id")
+	Sponsorship findSponsorshipById(int id);
+
+	@Query("select i from Invoice i where i.sponsorship.sponsor.id = :sponsorId")
+	Collection<Invoice> findAllInvoicesBySponsorId(int sponsorId);
+
+	@Query("select i from Invoice i where i.sponsorship.id = :sponsorshipId")
+	Collection<Invoice> findAllInvoicesBySponsorshipId(int sponsorshipId);
+
+	@Query("select s from Sponsor s where s.id = :id")
+	Sponsor findSponsorById(int id);
+}
